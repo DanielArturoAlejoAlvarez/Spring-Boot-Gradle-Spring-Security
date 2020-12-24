32a303b66747b5ff57b2dbac75f4d55bc1dc62e8
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2020-12-24 12:13:52 -0500

    Adding method AuthenticationManagerBuilder to security configuration

--- src/main/java/com/mediasoft/security/auth/services/MyUserDetailsService.java
+++ src/main/java/com/mediasoft/security/auth/services/MyUserDetailsService.java
@@ -1,13 +1,18 @@
 package com.mediasoft.security.auth.services;
 
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
+@Service
 public class MyUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        return new User("admin","password",new ArrayList<>());
     }
 }
