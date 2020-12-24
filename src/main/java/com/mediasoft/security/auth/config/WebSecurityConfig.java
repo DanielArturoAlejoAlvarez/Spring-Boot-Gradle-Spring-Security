2099307224e28c2d8567d73024d8c965cfd7065d
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2020-12-24 12:30:27 -0500

    Adding jsonwebtoken for build gradle project

--- src/main/java/com/mediasoft/security/auth/config/WebSecurityConfig.java
+++ src/main/java/com/mediasoft/security/auth/config/WebSecurityConfig.java
@@ -2,9 +2,12 @@ package com.mediasoft.security.auth.config;
 
 import com.mediasoft.security.auth.services.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -14,5 +17,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(myUserDetailsServer);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
