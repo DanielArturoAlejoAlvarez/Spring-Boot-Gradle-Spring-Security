f16d72930eb95f33534a1317358e7c6bbba092f8
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2020-12-24 12:54:06 -0500

    Adding method extractAllClaims to setting signing key using jwt

--- src/main/java/com/mediasoft/security/auth/utils/JwtUtil.java
+++ src/main/java/com/mediasoft/security/auth/utils/JwtUtil.java
@@ -1,9 +1,21 @@
 package com.mediasoft.security.auth.utils;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
 
 @Service
 public class JwtUtil {
 
     private String SECRET_KEY = "secret_key";
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+    }
+
+    
+
+
 }
