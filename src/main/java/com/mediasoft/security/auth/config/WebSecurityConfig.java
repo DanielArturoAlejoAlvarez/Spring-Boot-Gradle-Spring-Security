f3c72164b22308a478376d239b431522fe681381
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2020-12-25 11:20:03 -0500

    Adding method configure using HttpSecurity to permit authorization through authentication

--- src/main/java/com/mediasoft/security/auth/config/WebSecurityConfig.java
+++ src/main/java/com/mediasoft/security/auth/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import com.mediasoft.security.auth.services.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -17,6 +18,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(myUserDetailsServer);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/authenticate")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
     }
 
     @Bean
